ceb3633e2d7279e69847be2299ea5bf384f73346
mattdesl <dave.des@gmail.com>
2018-10-22 11:09:02 -0400

    more HUD utils

--- examples/fullscreen-shader.js
+++ examples/fullscreen-shader.js
@@ -1,57 +1,92 @@
-const canvasSketch = require('canvas-sketch');
-const createShader = require('canvas-sketch-util/shader');
-const glsl = require('glslify');
+const regl = require('regl');
 
-// Setup our sketch
-const settings = {
-  context: 'webgl',
-  animate: true
-};
-
-// Your glsl code
-const frag = glsl(`
-  precision highp float;
-
-  uniform float time;
-  uniform float aspect;
-  varying vec2 vUv;
-
-  #pragma glslify: noise = require('glsl-noise/simplex/4d');
-
-  void main () {
-    vec2 uv = vUv - 0.5;
-    uv.x *= aspect;
-
-    float anim = sin(time) * 0.5 + 0.5;
-
-    float a = 0.05;
-    float b = 1.0;
-    float c = 0.5;
-    uv.x += a * noise(vec4(uv.xy * b, time * c, -1.0));
-    uv.y += a * noise(vec4(uv.xy * b, time * c, 1.0));
-
-    float len = mod(fract(length(uv) * 5.0) + time * 0.25, 1.0);
-
-    vec3 color = mix(vec3(0.25), vec3(0.5), len);
-    gl_FragColor = vec4(color, 1.0);
+let instance;
+setInterval(() => {
+  if (instance) {
+    
   }
-`);
+  const canvas = document.createElement('canvas');
+  const gl = canvas.getContext('webgl');
+  gl.canvas = null;
+  console.log('next');
+  // instance = regl({
+  //   gl
+  // });
+}, 200)
 
-// Your sketch, which simply returns the shader
-const sketch = ({ gl }) => {
-  // Create the shader and return it
-  return createShader({
-    // Pass along WebGL context
-    gl,
-    // Specify fragment and/or vertex shader strings
-    frag,
-    // Specify additional uniforms to pass down to the shaders
-    uniforms: {
-      // Expose props from canvas-sketch
-      time: ({ time }) => time,
-      aspect: ({ width, height }) => width / height
-    }
-  });
-};
+// const canvasSketch = require('canvas-sketch');
+// const createShader = require('canvas-sketch-util/shader');
+// const glsl = require('glslify');
 
-canvasSketch(sketch, settings);
+// // Setup our sketch
+// const settings = {
+//   context: 'webgl',
+//   animate: true,
+//   params: {
+//     color1: {
+//       type: 'color',
+//       value: 'red',
+//       format: 'rgb-float'
+//     },
+//     color2: {
+//       type: 'color',
+//       value: 'blue',
+//       format: 'rgb-float'
+//     },
+//     slider: { type: 'range', value: 0.5 },
+//     foo: {
+//       value: 1
+//     }
+//   }
+// };
+
+// // Your glsl code
+// const frag = glsl(`
+//   precision highp float;
+
+//   uniform float time;
+//   uniform float aspect;
+//   uniform vec3 color1;
+//   uniform vec3 color2;
+//   varying vec2 vUv;
+
+//   #pragma glslify: noise = require('glsl-noise/simplex/4d');
+
+//   void main () {
+//     vec2 q = vUv - 0.5;
+//     q.x *= aspect;
+
+//     float t = noise(vec4(q * 2.0, time, 0.0)) * 0.5 + 0.5;
+
+//     float len = length(q);
+
+//     // d += t;
+//     float d = step(0.25 + t * 0.05, len);
+
+//     vec3 ball = color1;
+//     vec3 grad = mix(ball, color2, d);
+
+//     gl_FragColor = vec4(vec3(0.6), 1.0);
+//   }
+// `);
+
+// // Your sketch, which simply returns the shader
+// const sketch = ({ gl }) => {
+//   // Create the shader and return it
+//   return createShader({
+//     // Pass along WebGL context
+//     gl,
+//     // Specify fragment and/or vertex shader strings
+//     frag,
+//     // Specify additional uniforms to pass down to the shaders
+//     uniforms: {
+//       // Expose props from canvas-sketch
+//       time: ({ time }) => time,
+//       color1: ({ params }) => params.color1,
+//       color2: ({ params }) => params.color2,
+//       aspect: ({ width, height }) => width / height
+//     }
+//   });
+// };
+
+// canvasSketch(sketch, settings);
